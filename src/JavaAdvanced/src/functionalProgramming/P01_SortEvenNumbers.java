ebc45f1ca30fa956965d4f0b0db5b2fe95697d02
denislavtsenov <cenov.denislav@gmail.com>
2022-10-02 20:58:34 +0300

    SoftUni

---
+++ src/JavaAdvanced/src/functionalProgramming/P01_SortEvenNumbers.java
@@ -0,0 +1,22 @@
+package JavaAdvanced.src.functionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01_SortEvenNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).filter(e -> e % 2 == 0)
+                .toArray();
+
+        System.out.println(Arrays.toString(numbers).replace("[", "").replace("]", ""));
+
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers).replace("[", "").replace("]", ""));
+
+    }
+}
---
+++ src/JavaAdvanced/src/functionalProgramming/P03_CountUppercaseWords.java
@@ -0,0 +1,14 @@
+package JavaAdvanced.src.functionalProgramming;
+
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class P03_CountUppercaseWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputLine = scanner.nextLine().split("\\s+");
+
+
+    }
+}
