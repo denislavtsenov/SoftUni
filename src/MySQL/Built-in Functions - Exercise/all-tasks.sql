16c42d9edecc5df27ba62a7113a1baa5016ada33
Denislav Tsenov <cenov.denislav@gmail.com>
2023-05-29 20:03:34 +0300

    Update all-tasks.sql

--- src/MySQL/Built-in Functions - Exercise/all-tasks.sql
+++ src/MySQL/Built-in Functions - Exercise/all-tasks.sql
@@ -159,4 +159,4 @@ END AS 'Duration'
  `order_date`,
  DATE_ADD(`order_date`, INTERVAL 3 DAY) AS 'pay_due',
  DATE_ADD(`order_date`, INTERVAL 1 MONTH) AS 'deliver_due' 
- FROM `orders`;
+ FROM `orders`; FROM `orders`;
