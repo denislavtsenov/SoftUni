66bd6038b1adc828e38852d8a2984027ae00a269
Denislav Tsenov <cenov.denislav@gmail.com>
2023-05-29 20:07:51 +0300

    Update all-tasks.sql

--- src/MySQL/Built-in Functions - Exercise/all-tasks.sql
+++ src/MySQL/Built-in Functions - Exercise/all-tasks.sql
@@ -160,4 +160,5 @@ END AS 'Duration'
  `order_date`,
  DATE_ADD(`order_date`, INTERVAL 3 DAY) AS 'pay_due',
  DATE_ADD(`order_date`, INTERVAL 1 MONTH) AS 'deliver_due' 
+=======
 >>>>>>> Stashed changes
