974d1502b917de8927ae1f591e5d721d0f141d1a
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-05-31 16:14:31 +0300

    Data Aggregation - Exercise
    all-tasks.sql

--- src/MySQL/Data Aggregation - Exercise/all-tasks.sql
+++ src/MySQL/Data Aggregation - Exercise/all-tasks.sql
@@ -1,5 +1,6 @@
 -- 01. Records’ Count
 USE `gringotts`;
+-- 01. Records’ Count
 
 SELECT COUNT(*)
 FROM `wizzard_deposits`;
@@ -91,3 +92,94 @@ FROM `wizzard_deposits`
 WHERE `deposit_group` LIKE ('Troll Chest')
 GROUP BY `first_letter`
 ORDER BY `first_letter`;
+
+
+-- 11. Average Interest
+
+SELECT `deposit_group`, `is_deposit_expired`, AVG(`deposit_interest`) 
+FROM `wizzard_deposits`
+WHERE `deposit_start_date` > '1985/01/01'
+GROUP BY `deposit_group`, `is_deposit_expired`
+ORDER BY `deposit_group` DESC, `is_deposit_expired`;
+
+
+USE `soft_uni`;
+-- 12. Employees Minimum Salaries
+
+SELECT `department_id`, MIN(`salary`)
+FROM `employees`
+WHERE `department_id` IN (2,5,7) AND `hire_date` > 2000/01/01
+GROUP BY `department_id`
+ORDER BY `department_id`;
+
+
+-- 13. Employees Average Salaries
+
+CREATE TABLE `employees_earn_more_than_30000` AS
+SELECT * FROM `employees`
+WHERE `salary` > 30000;
+
+DELETE FROM `employees_earn_more_than_30000`
+WHERE `manager_id` = 42;
+
+UPDATE `employees_earn_more_than_30000`
+SET `salary` = `salary` + 5000
+WHERE `department_id` = 1;
+
+SELECT `department_id`, AVG(`salary`) AS 'avg_salary'
+FROM `employees_earn_more_than_30000`
+GROUP BY `department_id`
+ORDER BY `department_id`;
+
+
+-- 14. Employees Maximum Salaries
+
+SELECT `department_id`, MAX(`salary`) AS 'max_salary'
+FROM `employees`
+GROUP BY `department_id`
+HAVING `max_salary` < 30000 OR `max_salary` > 70000
+ORDER BY `department_id`;
+
+
+-- 15. Employees Count Salaries
+
+SELECT COUNT(*)
+FROM `employees`
+WHERE `manager_id` IS NULL;
+
+
+-- 16. 3rd Highest Salary
+
+SELECT DISTINCT
+    `department_id`, (
+		SELECT  DISTINCT `salary`
+        FROM `employees` AS e1
+        WHERE e1.`department_id` = `employees`.`department_id`
+        ORDER BY `salary` DESC
+        LIMIT 1 OFFSET 2) AS 'third_highest_salary'
+FROM
+    `employees`
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
+
+
+-- 17. Salary Challenge
+
+SELECT `first_name`, `last_name`, `department_id`
+FROM `employees` AS e1
+WHERE `salary` > (
+SELECT AVG(`salary`) 
+FROM `employees` AS e2
+WHERE e1.`department_id` = e2.`department_id`
+)
+ORDER BY `department_id`, `employee_id`
+LIMIT 10;
+
+
+-- 18. Departments Total Salaries
+
+SELECT `department_id`, SUM(`salary`) AS 'total_salary'
+FROM `employees`
+GROUP BY `department_id`
+ORDER BY `department_id`;
