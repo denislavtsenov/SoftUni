3a9d8ae34b07849654378c48df6ef8ee9370cd9d
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-06-01 16:37:52 +0300

    Table Relations - Lab
    2. Trip Organization

--- src/MySQL/Table Relations - Lab/all-tasks.sql
+++ src/MySQL/Table Relations - Lab/all-tasks.sql
@@ -13,3 +13,16 @@ CREATE TABLE `peaks`(
 CONSTRAINT fk_peaks_mountains
 FOREIGN KEY (`mountain_id`) REFERENCES `mountains`(id)
 );
+
+
+-- 2. Trip Organization
+
+SELECT 
+    `driver_id`,
+    `vehicle_type`,
+    CONCAT_WS(' ', `first_name`, `last_name`) AS 'driver_name'
+FROM
+    `vehicles`
+        JOIN
+    campers ON driver_id = campers.id;
+    
