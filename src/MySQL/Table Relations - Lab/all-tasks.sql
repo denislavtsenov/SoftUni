95687b25720d4e77ed960333c573b3d14e019f3c
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-04 16:35:32 +0300

    Update all-tasks.sql
    4. Delete Mountains

--- src/MySQL/Table Relations - Lab/all-tasks.sql
+++ src/MySQL/Table Relations - Lab/all-tasks.sql
@@ -36,3 +36,20 @@ r.`leader_id`,
 CONCAT_WS(' ', c.`first_name`, c.`last_name`) AS 'leader_name'
 FROM `routes` as r
 JOIN `campers` as c ON r.`leader_id` = c.id;
+
+
+-- 4. Delete Mountains
+
+CREATE TABLE `mountains`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(200)
+);
+
+CREATE TABLE `peaks` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(200),
+    `mountain_id` INT,
+    CONSTRAINT fk_peaks_mountains FOREIGN KEY (`mountain_id`)
+        REFERENCES `mountains` (id)
+        ON DELETE CASCADE
+);
