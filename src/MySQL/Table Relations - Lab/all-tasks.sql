dc47dd9af14ee6307f6937d64640947aaa373584
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-04 16:17:33 +0300

    Update all-tasks.sql
    3. SoftUni Hiking

--- src/MySQL/Table Relations - Lab/all-tasks.sql
+++ src/MySQL/Table Relations - Lab/all-tasks.sql
@@ -26,3 +26,13 @@ FROM
         JOIN
     campers ON driver_id = campers.id;
     
+
+-- 3. SoftUni Hiking
+
+SELECT 
+r.`starting_point` AS 'route_starting_point',
+r.`end_point` AS 'route_ending_point',
+r.`leader_id`,
+CONCAT_WS(' ', c.`first_name`, c.`last_name`) AS 'leader_name'
+FROM `routes` as r
+JOIN `campers` as c ON r.`leader_id` = c.id;
