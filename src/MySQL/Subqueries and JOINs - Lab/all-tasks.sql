78252fe17d4c53264ed6362f3fec745fe90de80e
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-05 14:02:30 +0300

    Subqueries and JOINs - Lab
    2. Towns and Addresses

--- src/MySQL/Subqueries and JOINs - Lab/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Lab/all-tasks.sql
@@ -12,3 +12,16 @@ RIGHT JOIN `departments` AS d
 ON d.`manager_id` = e.`employee_id`
 ORDER BY `employee_id`
 LIMIT 5;
+
+
+-- 2. Towns and Addresses
+
+SELECT
+t.`town_id`,
+t.`name` AS 'town_name',
+a.`address_text`
+FROM `towns` AS t
+JOIN `addresses` AS a 
+ON a.`town_id` = t.`town_id`
+WHERE t.`name` IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY `town_id`, `address_id`;
