d163c52694e03830dd6466a137d2b7f821e05794
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-05 14:43:32 +0300

    Subqueries and JOINs - Exercise
    03. Sales Employee

--- src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
@@ -28,3 +28,18 @@ JOIN `towns` AS t
 ON t.`town_id` = a.`town_id`
 ORDER BY `first_name`, `last_name`
 LIMIT 5;
+
+
+-- 03. Sales Employee
+
+SELECT 
+e.`employee_id`,
+e.`first_name`,
+e.`last_name`,
+d.`name` AS 'department_name'
+FROM `employees` AS e
+JOIN `departments` AS d
+ON d.`department_id` = e.`department_id`
+WHERE d.`name` IN ('Sales')
+ORDER BY `employee_id` DESC;
+
