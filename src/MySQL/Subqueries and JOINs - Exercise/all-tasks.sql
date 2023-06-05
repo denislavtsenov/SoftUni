b2177eb45657d9621e4a4e18c672d11498e3b160
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-05 16:24:38 +0300

    Subqueries and JOINs - Exercise
    08. Employee 24

--- src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
@@ -106,4 +106,21 @@ ORDER BY e.`first_name`, p.`name`
 LIMIT 5;
 
 
+-- 08. Employee 24
 
+SELECT
+e.`employee_id`,
+e.`first_name`,
+CASE
+WHEN YEAR(p.`start_date`) >= '2005' THEN NULL
+ELSE p.`name`
+END AS 'project_name'
+FROM `employees` AS e
+JOIN `employees_projects` AS ep
+ON ep.`employee_id` = e.`employee_id`
+JOIN `projects` AS p
+ON ep.`project_id` = p.`project_id`
+WHERE e.`employee_id` = 24
+ORDER BY `project_name`;
+
+ 
