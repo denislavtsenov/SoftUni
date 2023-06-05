abc9a0fb18ad5d9409b304993d9b7d4ec834faa5
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-05 14:37:46 +0300

    Subqueries and JOINs - Exercise
    02. Addresses with Towns

--- src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
@@ -13,3 +13,18 @@ ON a.`address_id` = e.`address_id`
 ORDER BY a.`address_id`
 LIMIT 5;
 
+
+-- 02. Addresses with Towns
+
+SELECT
+e.`first_name`,
+e.`last_name`,
+t.`name` AS 'town',
+a.`address_text`
+FROM `employees` AS e
+JOIN `addresses` AS a 
+ON a.`address_id` = e.`address_id`
+JOIN `towns` AS t
+ON t.`town_id` = a.`town_id`
+ORDER BY `first_name`, `last_name`
+LIMIT 5;
