fe790b2cdf7034c21cf44e54f36da441171f28f8
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-05 16:41:47 +0300

    Subqueries and JOINs - Exercise
    09. Employee Manager

--- src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
@@ -124,3 +124,16 @@ WHERE e.`employee_id` = 24
 ORDER BY `project_name`;
 
  
+-- 09. Employee Manager
+
+SELECT
+e.`employee_id`,
+e.`first_name`,
+e.`manager_id`,
+m.`first_name`
+FROM `employees` AS e
+JOIN `employees` AS m
+ON e.`manager_id` = m.`employee_id`
+WHERE e.`manager_id` IN (3, 7)
+ORDER BY e.`first_name`;
+
