946c9abe3b00664dcb60bd1d6194b37b1fca64fb
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-05 14:47:48 +0300

    Subqueries and JOINs - Exercise
    04. Employee Departments

--- src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
@@ -43,3 +43,18 @@ ON d.`department_id` = e.`department_id`
 WHERE d.`name` IN ('Sales')
 ORDER BY `employee_id` DESC;
 
+
+-- 04. Employee Departments
+
+SELECT 
+e.`employee_id`,
+e.`first_name`,
+e.`salary`,
+d.`name` AS 'department_name'
+FROM `employees` AS e 
+JOIN `departments` AS d
+ON d.`department_id` = e.`department_id`
+WHERE e.`salary` > 15000
+ORDER BY d.`department_id` DESC
+LIMIT 5;
+
