f45577ec978273321f305f344e0b0005cb74baa4
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 15:36:29 +0300

    Database Programmability - Exercise
    06. Employees by Salary Level

--- src/MySQL/Database Programmability - Exercise/all-tasks.sql
+++ src/MySQL/Database Programmability - Exercise/all-tasks.sql
@@ -81,3 +81,17 @@ END$$
 DELIMITER ;
 ;
 
+
+-- 06. Employees by Salary Level
+
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(50))
+	BEGIN
+		SELECT `first_name`, `last_name`
+		FROM `employees`
+		WHERE ufn_get_salary_level(`salary`) LIKE salary_level
+		ORDER BY `first_name` DESC, `last_name` DESC;
+    END$$
+DELIMITER ;
+;
+
