25a8a00318b16743c84c2f9b2635293f79a5db4f
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 10:03:41 +0300

    Subqueries and JOINs - Exercise
    13. Count Mountain Ranges

--- src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
@@ -183,3 +183,18 @@ ON p.`mountain_id` = m.`id`
 WHERE c.`country_code` IN ('BG') AND p.`elevation` > 2835
 ORDER BY p.`elevation` DESC;
 
+
+-- 13. Count Mountain Ranges
+
+SELECT
+c.`country_code`,
+COUNT(m.`mountain_range`) AS 'mountain_range'
+FROM `countries` AS c
+JOIN `mountains_countries` AS mc
+ON mc.`country_code` = c.`country_code`
+JOIN `mountains` AS m
+ON m.`id` = mc.`mountain_id`
+WHERE c.`country_code` IN ('BG', 'RU', 'US')
+GROUP BY c.`country_code`
+ORDER BY `mountain_range` DESC;
+
