87a4c3cbfeeb0e29584bd4cede1c73b1efb4b316
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 15:39:56 +0300

    Database Programmability - Exercise
    07. Define Function

--- src/MySQL/Database Programmability - Exercise/all-tasks.sql
+++ src/MySQL/Database Programmability - Exercise/all-tasks.sql
@@ -95,3 +95,15 @@ CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(50))
 DELIMITER ;
 ;
 
+
+-- 07. Define Function
+
+DELIMITER &&
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters VARCHAR(50), word VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+END&&  
+DELIMITER ;
+
