1dd811e7932bdb4cd3a778fd10029f464fca7770
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 12:15:19 +0300

    Database Programmability - Lab
    3. Employees Promotion By ID

--- src/MySQL/Database Programmability - Lab/all-tasks.sql
+++ src/MySQL/Database Programmability - Lab/all-tasks.sql
@@ -36,3 +36,21 @@ END$$
 DELIMITER ;
 ;
 
+
+-- 3. Employees Promotion By ID
+
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salary_by_id(id INT) 
+ BEGIN
+ START TRANSACTION;
+ IF ((SELECT COUNT(`employee_id`) FROM `employees` WHERE `employee_id` = id) <> 1)
+ THEN ROLLBACK;
+ ELSE
+	UPDATE `employees` AS e
+	SET e.`salary` = e.`salary` * 1.05
+    WHERE e.`employee_id` = id;
+    END IF;
+END$$
+DELIMITER ;
+;
+
