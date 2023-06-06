fb36c0c7fe7473f7d26bc2317bab96c653765bfe
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 14:35:41 +0300

    Database Programmability - Exercise
    02. Employees with Salary Above Number

--- src/MySQL/Database Programmability - Exercise/all-tasks.sql
+++ src/MySQL/Database Programmability - Exercise/all-tasks.sql
@@ -13,3 +13,18 @@ END$$
 DELIMITER ;
 ;
 
+
+-- 02. Employees with Salary Above Number
+
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above(needed_salary DECIMAL(19,4))
+BEGIN
+		SELECT `first_name`, `last_name`
+        FROM `employees`
+        WHERE `salary` >= needed_salary
+        ORDER BY `first_name`, `last_name`, `employee_id`;
+END$$
+DELIMITER ;
+;
+
+
