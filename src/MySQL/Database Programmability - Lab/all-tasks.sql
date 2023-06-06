7b21a2691129a14bd0decde7d147efc3018ed49a
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 14:14:12 +0300

    Database Programmability - Lab
    4. Triggered

--- src/MySQL/Database Programmability - Lab/all-tasks.sql
+++ src/MySQL/Database Programmability - Lab/all-tasks.sql
@@ -54,3 +54,31 @@ END$$
 DELIMITER ;
 ;
 
+
+-- 4. Triggered
+
+CREATE TABLE deleted_employees(
+`employee_id` INT AUTO_INCREMENT PRIMARY KEY, 
+`first_name` VARCHAR(50),
+`last_name` VARCHAR(50),
+`middle_name` VARCHAR(50),
+`job_title` VARCHAR(50),
+`department_id` INT,
+`salary` DECIMAL(19,2)
+);
+
+DELIMITER $$
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON `employees`
+FOR EACH ROW
+BEGIN
+INSERT INTO `deleted_employees`
+(`first_name`, `last_name`, `middle_name`, `job_title`, `department_id`, `salary`)
+VALUES (OLD.`first_name`, OLD.`last_name`, OLD.`middle_name`, OLD.`job_title`, OLD.`department_id`, OLD.`salary`);
+END$$
+DELIMITER ;
+;
+
+DELETE FROM `employees`
+WHERE `employee_id` = 1;
