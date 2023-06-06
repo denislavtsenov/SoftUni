34ae3237324abf2e7834100ee3ade3ed507f4c36
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 11:51:57 +0300

    Database Programmability - Lab
    2. Employees Promotion

--- src/MySQL/Database Programmability - Lab/all-tasks.sql
+++ src/MySQL/Database Programmability - Lab/all-tasks.sql
@@ -20,3 +20,19 @@ END$$
 DELIMITER ;
 ;
 
+
+-- 2. Employees Promotion
+
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR(50))
+ BEGIN
+	UPDATE `employees` AS e
+	JOIN `departments` AS d
+	ON e.`department_id` = d.`department_id`
+	SET e.`salary` = e.`salary` * 1.05
+	WHERE d.`name` = department_name
+	ORDER BY e.`first_name`, e.`salary`;
+END$$
+DELIMITER ;
+;
+
