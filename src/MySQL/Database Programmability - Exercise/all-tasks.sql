e882a3529d5658a9d8a120db7c4418ce26c99a9f
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 14:48:50 +0300

    Database Programmability - Exercise
    04. Employees from Town

--- src/MySQL/Database Programmability - Exercise/all-tasks.sql
+++ src/MySQL/Database Programmability - Exercise/all-tasks.sql
@@ -41,3 +41,23 @@ END$$
 DELIMITER ;
 ;
 
+
+-- 04. Employees from Town
+
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR(50))
+BEGIN
+	SELECT 
+	`first_name`,
+	`last_name`
+	FROM `employees` AS e
+	JOIN `addresses` AS a
+	ON a.`address_id` = e.`address_id`
+	JOIN `towns` AS t
+	ON t.`town_id` = a.`town_id`
+	WHERE t.`name` IN (town_name)
+	ORDER BY e.`first_name`, e.`last_name`, e.`employee_id`;
+END$$
+DELIMITER ;
+;
+
