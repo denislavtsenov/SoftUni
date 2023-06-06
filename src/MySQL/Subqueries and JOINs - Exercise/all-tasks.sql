2e41716fa49cb0f8412b3272132d4306b11c9731
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 09:54:11 +0300

    Subqueries and JOINs - Exercise
    12. Highest Peaks in Bulgaria

--- src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
+++ src/MySQL/Subqueries and JOINs - Exercise/all-tasks.sql
@@ -164,3 +164,22 @@ GROUP BY `department_id`
 ORDER BY `min_average_salary`
 LIMIT 1;
 
+
+USE `geography`;
+-- 12. Highest Peaks in Bulgaria
+
+SELECT 
+c.`country_code`,
+m.`mountain_range`,
+p.`peak_name`,
+p.`elevation`
+FROM `countries` AS c
+JOIN `mountains_countries` AS mc
+ON mc.`country_code` = c.`country_code`
+JOIN `mountains` AS m
+ON m.`id` = mc.`mountain_id`
+JOIN `peaks` AS p
+ON p.`mountain_id` = m.`id`
+WHERE c.`country_code` IN ('BG') AND p.`elevation` > 2835
+ORDER BY p.`elevation` DESC;
+
