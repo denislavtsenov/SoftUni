023d95ae3cc1bc15c4297087d07dd984e764ff3a
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-06 15:57:43 +0300

    Database Programmability - Exercise
    10. Future Value Function

--- src/MySQL/Database Programmability - Exercise/all-tasks.sql
+++ src/MySQL/Database Programmability - Exercise/all-tasks.sql
@@ -119,3 +119,17 @@ CREATE PROCEDURE usp_get_holders_full_name()
 	END$$
 DELIMITER ;
 
+
+-- 10. Future Value Function
+
+DELIMITER $$
+CREATE FUNCTION ufn_calculate_future_value(sum DECIMAL(19,4), interest_rate DECIMAL(10,2), years INT)
+RETURNS DECIMAL(19,4)
+DETERMINISTIC
+	BEGIN
+		DECLARE future_value DECIMAL(19,4);
+		SET future_value := sum * POW((1 + interest_rate), years);
+        RETURN future_value;
+	END$$ 
+DELIMITER ;
+
