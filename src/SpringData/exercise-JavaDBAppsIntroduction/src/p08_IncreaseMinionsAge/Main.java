1997a57df6ea60dd67bd7c3172717eb300a1d7ce
Denislav Tsenov <cenov.denislav@gmail.com>
2023-06-18 21:24:07 +0300

    DB App Introduction - exercises
    Taks 8

---
+++ src/SpringData/exercise-JavaDBAppsIntroduction/src/p08_IncreaseMinionsAge/Main.java
@@ -0,0 +1,47 @@
+package p08_IncreaseMinionsAge;
+
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Properties props = new Properties();
+        props.setProperty("user", "root");
+        props.setProperty("password", "1234");
+
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", props);
+
+        Scanner scanner = new Scanner(System.in);
+        int[] minionIds = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < minionIds.length; i++) {
+
+            int currentId = minionIds[i];
+
+            PreparedStatement updateStatement = connection.prepareStatement(
+                    "UPDATE `minions` " +
+                            "SET `name` = LOWER(`name`), " +
+                            "`age` = `age` + 1 " +
+                            "WHERE `id` IN (?);");
+            updateStatement.setInt(1, currentId);
+
+            updateStatement.executeUpdate();
+        }
+
+        PreparedStatement getAllMinions = connection.prepareStatement(
+                "SELECT `name`, `age` FROM `minions`;");
+
+        ResultSet minionsResultSet = getAllMinions.executeQuery();
+
+        while (minionsResultSet.next()) {
+            String name = minionsResultSet.getString("name");
+            int age = minionsResultSet.getInt("age");
+
+            System.out.printf("%s %d%n", name, age);
+        }
+    }
+}
