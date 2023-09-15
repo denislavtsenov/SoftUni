98bb2aa24f35e9fa4f02ff19fb2748ff1651f4e0
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-09-15 16:50:40 +0300

    Linear Data Structures - Lab
    Queue implemented with Node

--- src/DataStructuresFundamentals/Linear Data Structures - Lab/src/main/java/implementations/Queue.java
+++ src/DataStructuresFundamentals/Linear Data Structures - Lab/src/main/java/implementations/Queue.java
@@ -6,33 +6,99 @@ import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
 
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        Node(E element) {
+            this.element = element;
+        }
+    }
+
+    public Queue() {
+        this.head = null;
+        this.size = 0;
+    }
+
     @Override
     public void offer(E element) {
+        Node<E> toInsert = new Node<>(element);
+
+        if (this.isEmpty()) {
+            this.head = toInsert;
+        } else {
+            Node<E> current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = toInsert;
+        }
+        this.size++;
 
     }
 
     @Override
     public E poll() {
-        return null;
+        ensureNonEmpty();
+
+        E element = this.head.element;
+
+        if (this.size == 1) {
+            this.head = null;
+        } else {
+            Node<E> next = this.head.next;
+            this.head.next = null;
+            this.head = next;
+        }
+        this.size--;
+
+        return element;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+        return this.head.element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return this.current.next != null;
+            }
+
+            @Override
+            public E next() {
+                E value = this.current.element;
+                this.current = this.current.next;
+                return value;
+            }
+        };
+    }
+
+    private void ensureNonEmpty() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
     }
 }
