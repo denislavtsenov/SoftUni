0fd1d6f6ed3faaf335eb548cc272d38d58f9939a
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-09-18 16:36:18 +0300

    Linear Data Structures - Lab
    SinglyLinkedList Implementation

--- src/DataStructuresFundamentals/Linear Data Structures - Lab/src/main/java/implementations/SinglyLinkedList.java
+++ src/DataStructuresFundamentals/Linear Data Structures - Lab/src/main/java/implementations/SinglyLinkedList.java
@@ -5,20 +5,63 @@ import interfaces.LinkedList;
 import java.util.Iterator;
 
 public class SinglyLinkedList<E> implements LinkedList<E> {
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        Node(E element) {
+            this.element = element;
+        }
+    }
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
 
     @Override
     public void addFirst(E element) {
 
+        Node<E> nextElement = new Node<>(element);
+
+        nextElement.next = this.head;
+        this.head = nextElement;
+
+        this.size++;
     }
 
     @Override
-    public void addLast(E element) {
+    public void addLast(E element)   {
+        Node<E> lastElement = new Node<>(element);
 
+        if (this.isEmpty()) {
+            this.head = lastElement;
+        } else {
+            Node<E> current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = lastElement;
+
+        }
+        this.size++;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        ensureNonEmpty();
+
+        E element = this.head.element;
+
+        this.head = this.head.next;
+        this.size--;
+
+        return element;
     }
 
     @Override
@@ -28,7 +71,9 @@ public class SinglyLinkedList<E> implements LinkedList<E> {
 
     @Override
     public E getFirst() {
-        return null;
+        ensureNonEmpty();
+
+        return this.head.element;
     }
 
     @Override
@@ -38,16 +83,23 @@ public class SinglyLinkedList<E> implements LinkedList<E> {
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+
+    private void ensureNonEmpty() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
     }
 }
