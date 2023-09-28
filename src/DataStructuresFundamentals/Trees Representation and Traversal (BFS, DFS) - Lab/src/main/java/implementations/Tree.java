f964395c70595eb2b99f24ad2c503efc9ac2f45a
Denislav Tsenov <cenov.denislav@gmail.com>
2023-09-28 20:32:02 +0300

    Trees Representation and Traversal (BFS, DFS) - Lab
    DFS and BFS implementation

--- src/DataStructuresFundamentals/Trees Representation and Traversal (BFS, DFS) - Lab/src/main/java/implementations/Tree.java
+++ src/DataStructuresFundamentals/Trees Representation and Traversal (BFS, DFS) - Lab/src/main/java/implementations/Tree.java
@@ -2,7 +2,9 @@ package implementations;
 
 import interfaces.AbstractTree;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Tree<E> implements AbstractTree<E> {
@@ -17,28 +19,53 @@ public class Tree<E> implements AbstractTree<E> {
         this.parent = null;
         this.children = new ArrayList<>();
 
-        for(Tree<E> child : children) {
-          this.children.add(child);
-          child.parent = this;
+        for (Tree<E> child : children) {
+            this.children.add(child);
+            child.parent = this;
         }
     }
 
     @Override
     public List<E> orderBfs() {
-        return null;
+        List<E> result = new ArrayList<>();
+
+        Deque<Tree<E>> queue = new ArrayDeque<>();
+        queue.offer(this);
+
+        while (queue.size() > 0) {
+            Tree<E> current = queue.poll();
+            result.add(current.key);
+
+            for (Tree<E> child : current.children) {
+                queue.offer(child);
+            }
+        }
+        return result;
     }
 
     @Override
     public List<E> orderDfs() {
-        return null;
+
+        List<E> order = new ArrayList<>();
+
+        this.dfs(this, order);
+        return order;
+    }
+
+    private void dfs(Tree<E> tree, List<E> order) {
+        for (Tree<E> child : tree.children) {
+            this.dfs(child, order);
+        }
+
+        order.add(tree.key);
     }
 
     @Override
     public void addChild(E parentKey, Tree<E> child) {
 
     }
-	
-	@Override
+
+    @Override
     public void removeNode(E nodeKey) {
 
     }
