6ddd4c1a9b63f77f6c5efa555c29a3e5e5c6f209
Denislav Tsenov <cenov.denislav@gmail.com>
2023-10-01 22:04:12 +0300

    Trees Representation and Traversal (BFS and DFS) - Exercises
    Matrix recursive implementation

---
+++ src/DataStructuresFundamentals/Trees Representation and Traversal (BFS, DFS) - Exercises/src/main/java/implementations/TheMatrix.java
@@ -0,0 +1,51 @@
+package implementations;
+
+public class TheMatrix {
+    private char[][] matrix;
+    private char fillChar;
+    private char startChar;
+    private int startRow;
+    private int startCol;
+
+    public TheMatrix(char[][] matrix, char fillChar, int startRow, int startCol) {
+        this.matrix = matrix;
+        this.fillChar = fillChar;
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.startChar = this.matrix[this.startRow][this.startCol];
+    }
+
+    public void solve() {
+        fillMatrix(this.startRow, this.startCol);
+    }
+
+    private void fillMatrix(int row, int col) {
+
+        if (isOutOfBounds(row, col) || this.matrix[row][col] != this.startChar) {
+            return;
+        }
+
+        this.matrix[row][col] = this.fillChar;
+
+        this.fillMatrix(row + 1, col);
+        this.fillMatrix(row, col + 1);
+        this.fillMatrix(row - 1, col);
+        this.fillMatrix(row, col - 1);
+    }
+
+    private boolean isOutOfBounds(int row, int col) {
+        return row < 0 || row >= this.matrix.length || col < 0 || col >= this.matrix[row].length;
+    }
+
+    public String toOutputString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int row = 0; row < this.matrix.length; row++) {
+            for (int col = 0; col < this.matrix[row].length; col++) {
+                stringBuilder.append(this.matrix[row][col]);
+            }
+            stringBuilder.append(System.lineSeparator());
+        }
+        return stringBuilder.toString().trim();
+    }
+}
---
+++ src/DataStructuresFundamentals/Trees Representation and Traversal (BFS, DFS) - Exercises/src/main/java/implementations/Tree.java
@@ -0,0 +1,94 @@
+package implementations;
+
+import interfaces.AbstractTree;
+
+import java.util.*;
+
+public class Tree<E> implements AbstractTree<E> {
+
+    private E key;
+    private Tree<E> parent;
+    private List<Tree<E>> children;
+
+    public Tree(E key, Tree<E>... children) {
+        this.key = key;
+        this.parent = null;
+        this.children = Arrays.asList(children);
+
+        for (Tree<E> child : children) {
+            child.setParent(this);
+            this.children.add(child);
+            child.parent = this;
+        }
+    }
+
+
+    @Override
+    public void setParent(Tree<E> parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void addChild(Tree<E> child) {
+        Deque<Tree<E>> queue = new ArrayDeque<>();
+        queue.offer(this);
+
+        while (queue.size() > 0) {
+            Tree<E> current = queue.poll();
+
+            for (Tree<E> c : current.children) {
+                c.children.add(child);
+                queue.offer(c);
+            }
+
+        }
+    }
+
+    @Override
+    public Tree<E> getParent() {
+        return this.parent;
+    }
+
+    @Override
+    public E getKey() {
+        return this.key;
+    }
+
+    @Override
+    public String getAsString() {
+        return null;
+    }
+
+    @Override
+    public List<E> getLeafKeys() {
+        return null;
+    }
+
+    @Override
+    public List<E> getMiddleKeys() {
+        return null;
+    }
+
+    @Override
+    public Tree<E> getDeepestLeftmostNode() {
+        return null;
+    }
+
+    @Override
+    public List<E> getLongestPath() {
+        return null;
+    }
+
+    @Override
+    public List<List<E>> pathsWithGivenSum(int sum) {
+        return null;
+    }
+
+    @Override
+    public List<Tree<E>> subTreesWithGivenSum(int sum) {
+        return null;
+    }
+}
+
+
+
