8f39b1d6594d40cfa6ceba97731f0c4fb31df37d
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-10-04 13:04:50 +0300

    Data Structures Fundamentals with Java - Regular Exam - 11 December 2022
    2. Airlines implementation

--- src/DataStructuresFundamentals/Exams/Regular Exam - 11 December 2022/src/main/java/org/softuni/exam/structures/AirlinesManagerImpl.java
+++ src/DataStructuresFundamentals/Exams/Regular Exam - 11 December 2022/src/main/java/org/softuni/exam/structures/AirlinesManagerImpl.java
@@ -3,35 +3,61 @@ package org.softuni.exam.structures;
 import org.softuni.exam.entities.Airline;
 import org.softuni.exam.entities.Flight;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class AirlinesManagerImpl implements AirlinesManager {
+
+    Map<String, Airline> airlines = new LinkedHashMap<>();
+    Map<String, Flight> flights = new LinkedHashMap<>();
+    Map<String, Flight> flightsByAirlines = new LinkedHashMap<>();
+
+
     @Override
     public void addAirline(Airline airline) {
-
+        airlines.put(airline.getId(), airline);
     }
 
     @Override
     public void addFlight(Airline airline, Flight flight) {
 
+        if (!contains(airline)) {
+            throw new IllegalArgumentException();
+        }
+
+        flights.put(flight.getId(), flight);
+        flightsByAirlines.put(airline.getId(), flight);
+
     }
 
     @Override
     public boolean contains(Airline airline) {
-        return false;
+        return airlines.get(airline.getId()) != null;
     }
 
     @Override
     public boolean contains(Flight flight) {
-        return false;
+        return flights.get(flight.getId()) != null;
     }
 
     @Override
     public void deleteAirline(Airline airline) throws IllegalArgumentException {
 
+        if (!contains(airline)) {
+            throw new IllegalArgumentException();
+        }
+
+        Flight flight = flightsByAirlines.get(airline.getId());
+        flights.remove(flight.getId());
+        
+        flightsByAirlines.remove(airline.getId());
+        airlines.remove(airline.getId());
+
     }
 
     @Override
     public Iterable<Flight> getAllFlights() {
-        return null;
+        return flights.values();
     }
 
     @Override
